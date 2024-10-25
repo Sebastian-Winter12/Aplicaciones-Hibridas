8180fa8dfd3fca07748516326180446dc843a0f4
Sebastian-Winter12 <Sebastian.Winter@davinci.edu.ar>
2024-10-25 10:09:27 -0300

    Se agrego un array de objetos y un sistema de cuenta

--- semana_11/src/App.jsx
+++ semana_11/src/App.jsx
@@ -1,46 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Card from './Card'
-import User from './User'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import Card from "./Card";
+import "./App.css";
 
-
-//  fetch para unir el backend y el frontend utilizando async await
-// Hay un error de permitsos (seguramente) qeu se resuelve en el index.js del backend, se agrega una linea de codigo que acepta permisos externos. app.use ( cors()) -> hay que usar npm install cors. Ademas hay que requerirlo con el const require.
-// async function getUser () {
-//   const endPoint = 'edpoinrt';
-//   const user = await fetch (endPoint);
-//   const users = await reps.json ();
-  
-//   console.log(users);
-// }
+import { useState, useEffect } from "react";
 
 function App() {
+  let logueado = true;
+
+  let mensaje = logueado == true ? "Bienvenido" : "Iniciar sesion";
+
   let nombre = "José";
-  let edad = 21;
-  const cursos = ['HTML', 'JS', 'React'];
+  const products = [
+    { id: 1, nombre: "Mate", precio: 500 },
+    { id: 1, nombre: "Glletitas", precio: 100 },
+    { id: 1, nombre: "Zapatilla", precio: 2500 },
+  ];
+  const cursos = ["HTML", "JS", "React"];
   let titulo = <h2> Esto es un título</h2>;
 
   const user = {
-    name: 'Juan',
-    email: 'juan@mail.com'
-  }
+    name: "Juan",
+    email: "juan@mail.com",
+  };
+
   return (
     <div>
       <h1> Aplicaciones Híbridas</h1>
       {/* Se puede pasar como objeto o con doble {{}} */}
-      <User data={ user.name }/>
-      <img src="" alt="" /> 
-      { titulo}
-      <Card texto="Termo" precio={40000} />
-      <Card texto="Mate" precio={5000} />
+      {titulo}
+      <h4> {mensaje} </h4>
 
-      <p className='verde'> Hola { nombre }</p>
-      <p> {  edad + 2  }</p>
-      <p> {  cursos[2]  } </p>
+      <hr />
+
+      <div className="row">
+        {products.map((producto) => {
+          return <Card texto={producto.nombre} precio={producto.precio} />;
+        })}
+      </div>
+
+      {/* Solo se puede retornar con div o <> </> v*/}
+      {logueado == true ? (
+        <>
+          <h4> Bienvenido {nombre} </h4>
+          <div className="row">
+            <Card texto="Termo" precio={40000} />
+            <Card texto="Mate" precio={5000} />
+          </div>
+        </>
+      ) : (
+        <h4>
+          {" "}
+          Iniciar Sesion <a href="#">Login</a>{" "}
+        </h4>
+      )}
+
+      <p className="verde"> Hola {nombre}</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
